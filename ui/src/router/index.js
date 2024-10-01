c4a5fc335732a3e262e2247fb6be6aec8b6a5916
Huseyin Ekemen <huseyin.ekemen@h2hsecure.com>
2024-10-01 03:23:27 +0300

    custom forbidden

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -16,6 +16,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/Queue.vue')
+    },
+    {
+      path: '/forbiden',
+      name: 'forbiden',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Queue.vue')
     }
   ]
 })
