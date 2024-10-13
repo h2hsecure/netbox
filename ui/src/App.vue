2ae417a648cd2de09b6f529e5dcd8cb5f5a102df
Huseyin Ekemen <huseyin.ekemen@h2hsecure.com>
2024-10-13 03:53:33 +0300

    fix i18n usage

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,21 +1,7 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import { onMounted } from 'vue'
-
-onMounted(() => {
-  const availibleLocales = this.$i18n.availableLocales;
-  const usersLanguage = window.navigator.language
-
-  if (availibleLocales.includes(usersLanguage)) {
-    this.$i18n.locale = usersLanguage;
-  } else {
-    this.$i18n.locale = 'en';
-  }
-})
 </script>
 
 <template>
-
-
   <RouterView />
 </template>
--- ui/src/ForbidenApp.vue
+++ ui/src/ForbidenApp.vue
@@ -1,18 +1,5 @@
 <script setup>
 import NotAllowed from './views/NotAllowed.vue'
-import { onMounted } from 'vue'
-
-
-onMounted(() => {
-  const availibleLocales = this.$i18n.availableLocales;
-  const usersLanguage = window.navigator.language
-
-  if (availibleLocales.includes(usersLanguage)) {
-    this.$i18n.locale = usersLanguage;
-  } else {
-    this.$i18n.locale = 'en';
-  }
-})
 </script>
 
 <template>
